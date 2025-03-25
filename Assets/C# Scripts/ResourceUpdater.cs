10a3bdffc9b72140f4220166cc27ad890f99d1d9
TwanRijnsburger <158170252+TwanRijnsburger@users.noreply.github.com>
2025-03-25 16:29:56 +0100

    ui ingame and script

--- Assets/C# Scripts/ResourceUpdater.cs
+++ Assets/C# Scripts/ResourceUpdater.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RecourceUpdater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshProUGUI[] naam;
+
+
+
+    private void Start()
     {
-        
+        ResourceManager.playerResourcesDataArray.OnValueChanged += (PlayerResourcesDataArray oldValue, PlayerResourcesDataArray newValue) => OnresourceUpdate(newValue);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnresourceUpdate(PlayerResourcesDataArray newValue)
     {
-        
+        int food = newValue.food[ClientManager.LocalClientGameId];
+        int stone = newValue.food[ClientManager.LocalClientGameId];
+        int wood = newValue.food[ClientManager.LocalClientGameId];
+        int gems = newValue.food[ClientManager.LocalClientGameId];
+
+        var iterator = 0;
+        var isTrue = true;
+        while (isTrue)
+        {
+            isTrue = false;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    iterator++;
+                    naam[iterator].text = food.ToString();
+                }
+                else if (i == 1)
+                {
+                    iterator++;
+                    naam[iterator].text = stone.ToString();
+                }
+                else if (i == 2)
+                {
+                    iterator++;
+                    naam[iterator].text = wood.ToString();
+                }
+                else 
+                {
+                    iterator++;
+                    naam[iterator].text = gems.ToString();
+                }
+            }
+        }
     }
 }
