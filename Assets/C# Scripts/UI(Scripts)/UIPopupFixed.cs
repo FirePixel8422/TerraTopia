298533e9ddba5e4eb65071e3e7b761cfbd7946ed
FirePixel8422 <bmtminecraft@hotmail.com>
2025-04-01 15:06:20 +0200

    UIPopup
    Added UIPopupFixed.cs
    Allows to display GameObjects under a canvas by fading them in and out if target gamobjects detects the mouse over itsself

---
+++ Assets/C# Scripts/UI(Scripts)/UIPopupFixed.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class UIPopupFixed : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("The UIPopup object that will be displayed")]
+    [SerializeField] private GameObject popupObj;
+
+
+    [Header("Fade UIPopup in and out instead of toggling it")]
+    [SerializeField] private bool useFading = true;
+
+    [Header("The time it takes for the UIPopup to fade in and out")]
+    [SerializeField] private float fadeTime = .15f;
+
+    private CanvasGroup canvasGroup;
+
+
+
+    private void Start()
+    {
+        popupObj.SetActive(false);
+
+        canvasGroup = popupObj.GetComponent<CanvasGroup>();
+
+        if (useFading && canvasGroup == null)
+        {
+            canvasGroup = popupObj.transform.AddComponent<CanvasGroup>();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData _)
+    {
+        if (useFading)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeInUI());
+        }
+        else
+        {
+            canvasGroup.alpha = 1;
+            popupObj.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData _)
+    {
+        if (useFading)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeOutUI());
+        }
+        else
+        {
+            popupObj.SetActive(false);
+        }
+    }
+
+
+
+    private IEnumerator FadeInUI()
+    {
+        popupObj.SetActive(true);
+
+        float elapsed = 0;
+        while (elapsed < fadeTime)
+        {
+            yield return null;
+
+            elapsed += Time.deltaTime;
+            canvasGroup.alpha = elapsed / fadeTime;
+        }
+
+        canvasGroup.alpha = 1;
+    }
+
+    private IEnumerator FadeOutUI()
+    {
+        float elapsed = fadeTime;
+        while (elapsed > 0)
+        {
+            yield return null;
+
+            elapsed -= Time.deltaTime;
+            canvasGroup.alpha = elapsed / fadeTime;
+        }
+
+        canvasGroup.alpha = 0;
+        popupObj.SetActive(false);
+    }
+}
